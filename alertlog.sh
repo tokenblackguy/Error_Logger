5073e7b23272a3b36ba3702f893e276b9481dec7
Token <41331974+tokenblackguy@users.noreply.github.com>
2020-09-09 10:12:06 +0300

    Create alertlog.sh
    $LOGFILEPATH will be monitored after getting values from syslog file.

---
+++ alertlog.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#Change variables before you do anything!
+#Do not change the variable names as PATH.
+
+MAIL=mail_address
+LOGFILEPATH=path_for_monitor
+SYSLOG=/var/log/syslog
+
+
+tail "$SYSLOG" > "$LOGFILEPATH"
+
+
+ALERT="$(egrep --color=always $@ -wi 'error|critical|warning'  "$LOGFILEPATH")"
+
+
+if [ -z "$ALERT" ]
+then
+      echo "\$ALERT is NULL"
+      #cat $LOGFILEPATH | mail -s 'NO ERRORS' $MAIL
+else
+      echo "\$ALERT is NOT NULL"
+      echo $ALERT | mail -s 'ERRORS' $MAIL
+fi
